ab9a60c52a0a6c2079c7e01de96b76b334db463a
Antti <anttiparhiala@hotmail.com>
2022-10-03 00:07:43 +0300

    Add project files.

---
+++ Lights.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Älytalo
+{
+    public class Lights
+    {
+        public bool Switced { get; private set; } = false;
+        //Set metodi on private ettei sitä voi vahingossa muuttaa tän luokan ulkopuolelta
+        private string Dimmer = "";
+        //Vahvan kapselloinnin takia vain Adjust metodi voi muuttaa Dimmerin arvoa
+        public void Adjust(string dimmer, Slider slid, Border bord, Button swit, TextBox box)
+            //Metodi muuttaa sliderin, borderin, txtboksin ja On/Off buttonin arvoa Dimmerin mukaan
+        {
+            Dimmer = dimmer;
+            if (Checker.IsDecimal(Dimmer) || Checker.IsNumeric(Dimmer))
+                //Varmistetaan että dimmerin arvo on numeroita tai desimaaleja
+            {
+                double dim = double.Parse(Dimmer);
+                //Muutetaan dimmerin arvo doubleks
+                slid.Value = dim;
+                //Asetetaan Sliderin arvo Dimmerin mukaan
+                bord.Background.Opacity = dim / 100;
+                //Asetetaan border objektin alpha värikanava, valon vahvuus effektiä varten 
+                box.Text = dim.ToString("F0") + "%";
+                //Asetetaan tekstiboksiin valon vahvuus prosentteina ilman desimaaleja
+                if (slid.Value == 0)
+                {
+                    swit.Content = "Off"; 
+                    swit.Background = Brushes.Red;
+                    Switced = false;
+                }
+                else
+                {
+                    swit.Content = "On";
+                    swit.Background = Brushes.Green;
+                    Switced = true;
+                }
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Älytalo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Lights keittioL = new();
+        Lights olohuoneL = new();
+        Lights saunaL = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void SliderLights_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+            //Hoitaa kaikista slidereista lähtevät muutokset
+        {
+            if (sender == SlidKeittio)
+            {
+                keittioL.Adjust(SlidKeittio.Value.ToString(),SlidKeittio, BordKeittio, BtnKeittioVal,
+                    TxtKeittioVal);
+            }
+            if (sender == SlidOlohuone)
+            {
+                olohuoneL.Adjust(SlidOlohuone.Value.ToString(),SlidOlohuone, BordOlohuone,
+                    BtnOlohuoneVal, TxtOlohuoneVal);
+            }
+            if (sender == SlidSauna)
+            {
+                saunaL.Adjust(SlidSauna.Value.ToString(), SlidSauna, BordSauna, BtnSaunaVal,
+                    TxtSaunaVal);
+            }
+        }
+
+        private void BtnLights_Click(object sender, RoutedEventArgs e)
+            //Hoitaa buttoneista lähtevät muutokset
+        {
+            if (sender == BtnKeittioVal)
+            {
+                if (keittioL.Switced)
+                {
+                    keittioL.Adjust("0", SlidKeittio, BordKeittio, BtnKeittioVal, TxtKeittioVal);
+                }
+            }
+            if (sender == BtnOlohuoneVal)
+            {
+                if (olohuoneL.Switced)
+                {
+                    olohuoneL.Adjust("0", SlidOlohuone, BordOlohuone, BtnOlohuoneVal, TxtOlohuoneVal);
+                }
+            }
+            if (sender == BtnSaunaVal)
+            {
+                if (saunaL.Switced)
+                {
+                    saunaL.Adjust("0", SlidSauna, BordSauna, BtnSaunaVal, TxtSaunaVal);
+                }
+            }
+        }
+
+        private void TxtLights_LostFocus(object sender, RoutedEventArgs e)
+            //Hoitaa tekstikenttistä lähtevät muutokset, tekstikenttiin voi manuaalisesti laittaa
+            //valaistuksen vahvuuden, päivittyy kun focus siirtyy pois tekstikentästä
+        {
+            if (sender == TxtKeittioVal)
+            {
+                LightTextSettingRoutine(keittioL, TxtKeittioVal, SlidKeittio, BordKeittio, BtnKeittioVal);
+            }
+            if (sender == TxtOlohuoneVal)
+            {
+                LightTextSettingRoutine(olohuoneL, TxtOlohuoneVal, SlidOlohuone, BordOlohuone,
+                    BtnOlohuoneVal);
+            }
+            if (sender == TxtSaunaVal)
+            {
+                LightTextSettingRoutine(saunaL, TxtSaunaVal, SlidSauna, BordSauna, BtnSaunaVal);
+            }
+        }
+        void LightTextSettingRoutine(Lights light, TextBox txt, Slider slid, Border bord, Button btn)
+            //Sisennys funktio tekstikentistä lähteviin muutoksiin
+        {
+            string temp = txt.Text.Replace('%', ' ').TrimEnd();
+            if (Checker.IsNumeric(temp, 0, 100))
+            {
+                light.Adjust(temp, slid, bord, btn, txt);
+            }
+            else
+            {
+                txt.Text = slid.Value.ToString("F0") + "%";
+            }
+        }
+    }
+}
